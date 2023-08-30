ac95fd39ced7e3f8f1ec3bffaac2f1444f4794eb
accenggg <arseliusngiklan@gmail.com>
2023-08-30 19:00:04 +0800

    init commit - code sudah passed semua

--- scripts/filterCarByAvailability.js
+++ scripts/filterCarByAvailability.js
@@ -1,12 +1,108 @@
 function filterCarByAvailability(cars) {
   // Sangat dianjurkan untuk console.log semua hal hehe
-  console.log(cars);
+  // console.log(cars);
 
   // Tempat penampungan hasil
   const result = [];
 
   // Tulis code-mu disini
+  // 1) inisialisasi variabel x = 0 untuk digunakan sebagai index pada penambahan data ke result
+  let x = 0
+
+  // 2) perulangan untuk akses data secara satu persatu
+  for(let i=0; i<cars.length; i++){
+    // 3) mengecek setiap data yang memiliki key available dengan value true
+    if(cars[i].available === true){
+      // 4) menambahkan data ke result dengan index x
+      result[x] = cars[i]
+      // 5) menambahkan jumlah x sebesar 1 untuk maju ke index selanjutnya
+      x += 1
+    }
+  }
 
   // Rubah code ini dengan array hasil filter berdasarkan availablity
   return result;
 }
+
+const dataFilters = [
+  {
+    "id": "6e2bc663-5197-441a-957b-bc75e4a2da7c",
+    "plate": "DBH-3491",
+    "transmission": "Automatic",
+    "manufacture": "Ford",
+    "model": "F150",
+    "available": true,
+    "type": "Sedan",
+    "year": 2022,
+    "options": [
+      "Cruise Control",
+      "Tinted Glass",
+      "Tinted Glass",
+      "Tinted Glass",
+      "AM/FM Stereo"
+    ],
+    "specs": [
+      "Brake assist",
+      "Leather-wrapped shift knob",
+      "Glove box lamp",
+      "Air conditioning w/in-cabin microfilter",
+      "Body color folding remote-controlled pwr mirrors",
+      "Dual-stage front airbags w/occupant classification system"
+    ]
+  },
+  {
+    "id": "9ff03bbc-b18c-4ba7-8f3a-4c4b5c2f6c77",
+    "plate": "WXB-3984",
+    "transmission": "Automatic",
+    "manufacture": "BMW",
+    "model": "X5",
+    "available": false,
+    "type": "Convertible",
+    "year": 2019,
+    "options": [
+      "Keyless Entry",
+      "Power Windows",
+      "MP3 (Single Disc)",
+      "CD (Multi Disc)",
+      "Navigation"
+    ],
+    "specs": [
+      "Rear passenger map pockets",
+      "Electrochromic rearview mirror",
+      "Dual chrome exhaust tips",
+      "Locking glove box",
+      "Pwr front vented disc/rear drum brakes"
+    ]
+  },
+  {
+    "id": "bf6b5c43-1377-4ae0-8908-310c64266f81",
+    "plate": "OSL-4224",
+    "transmission": "Automanual",
+    "manufacture": "Lincoln",
+    "model": "MKZ",
+    "available": true,
+    "type": "Sedan",
+    "year": 2021,
+    "options": [
+      "Bucket Seats",
+      "Airbag: Passenger",
+      "Airbag: Driver",
+      "Power Seats",
+      "Airbag: Side",
+      "Antilock Brakes",
+      "CD (Multi Disc)"
+    ],
+    "specs": [
+      "Driver & front passenger map pockets",
+      "Direct-type tire pressure monitor system",
+      "Cargo area lamp",
+      "Glove box lamp",
+      "Silver finish interior door handles",
+      "Driver & front passenger advanced multistage airbags w/occupant sensors",
+      "Silver accent IP trim finisher -inc: silver shifter finisher",
+      "Fasten seat belt warning light/chime"
+    ]
+  }
+]
+
+console.log(filterCarByAvailability(dataFilters))
